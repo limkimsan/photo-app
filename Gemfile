e1ce95919a0e08ef2280942e950dcc28202481f9
Kimsanlim <kimsanlim27@gmail.com>
2022-08-04 00:29:52 +0700

    Generate scaffold of table Image

--- Gemfile
+++ Gemfile
@@ -54,7 +54,6 @@ gem "font-awesome-rails"
 gem 'stripe'
 gem 'carrierwave'
 gem 'mini_magick'
-gem 'fog'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_173233) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_172817) do
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
+
   create_table "payments", force: :cascade do |t|
     t.string "email"
     t.string "toke"
@@ -29,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_173233) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
